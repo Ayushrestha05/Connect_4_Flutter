08db9e6b092b8f2042633131fedb988b64e77ce6
Ayush Shrestha <ayushrestha05@gmail.com>
2022-01-07 17:01:17 +0545

    Added ChangeNotifierProvider

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,5 +1,7 @@
+import 'package:connect4_flutter/utilities/board.dart';
 import 'package:connect4_flutter/widgets/game_body.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
@@ -15,7 +17,16 @@ class _GameScreenState extends State<GameScreen> {
       appBar: AppBar(
         title: const Text('Connect 4 AI Demonstration'),
       ),
-      body: const GameBody(),
+      body: Center(
+        child: Column(
+          children: [
+            ChangeNotifierProvider<Board>(
+              create: (context) => Board(),
+              child: GameBody(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
